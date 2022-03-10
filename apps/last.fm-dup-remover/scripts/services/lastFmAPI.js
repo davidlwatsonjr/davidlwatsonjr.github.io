69b888a884e2775fdb9b0971bb2ff184c76ec7c0
David Watson <davidlwatsonjr@users.noreply.github.com>
2022-03-09 22:36:45 -0500

    Update lastFmAPI endpoint baseURL

--- apps/last.fm-dup-remover/scripts/services/lastFmAPI.js
+++ apps/last.fm-dup-remover/scripts/services/lastFmAPI.js
@@ -1,5 +1,5 @@
 angular.module('last.fmDupRemoverApp').factory('lastFM', ['$http', function ($http) {
-  var baseURL = 'https://davidlwatsonjr.com/apis/last.fm-dup-remover/index.php/';
+  var baseURL = 'https://last-fm-api-endpoint-voexvag3xa-uc.a.run.app/index.php/';
 
   return {
     getSession: function () {
